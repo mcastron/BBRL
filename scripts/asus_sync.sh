42a6583250ab47a22f253a7013c792d9afae2906
mcastron <m.castronovo@ulg.ac.be>
2014-12-01 19:39:51 +0100

    - Remove unwanted files from repository

--- scripts/asus_sync.sh
+++
@@ -1,4 +0,0 @@
-
-#!/bin/bash
-
-rsync -avz --exclude 'backups/' --exclude 'bin/*' --exclude '*.dSYM' --exclude '*~' --exclude 'CMakeFiles' --exclude 'CMakeCache*' --exclude '*make' --exclude 'Makefile' --exclude '*.a' --exclude '*.o' --exclude 'TinyBRL-DDS' --exclude 'TinyBRL-export' --exclude 'TinyBRL-zlib' ../TinyBRL/ asus-laptop:~/TinyBRL/
