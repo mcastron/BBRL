83693299c01577df89902981b96ed509905e668d
mcastron <m.castronovo@ulg.ac.be>
2015-08-27 14:50:18 +0200

    - Added 'XXXAgent.h' and 'XXXAgent.cpp' files.

---
+++ templates/XXXAgent.h
@@ -0,0 +1,175 @@
+
+/* TODO - replace "XXXAgent" the agent class name */
+
+
+#ifndef XXXAGENT_H
+#define XXXAGENT_H
+
+#include "Agent.h"
+#include "../dds.h"
+#include "../../ExternalLibs.h"
+
+
+// ===========================================================================
+/**
+	\class 	XXXAgent
+	\author 	Castronovo Michael
+	
+	\brief 	Interface of ??? Agent to address MDP problems for:
+				- DISCRETE state space (int)
+				- DISCRETE action space (int)
+				- SINGLE reward (double)
+
+               (TODO - to complete)
+
+	\date 	2014-09-11
+*/
+// ===========================================================================
+/* final */ class dds::XXXAgent : public dds::Agent
+{		
+	public:
+		// =================================================================
+		//	Public Constructor/Destructor
+		// =================================================================
+		/**
+			\brief	Constructor.
+			
+			\param[is	The 'ifstream' containting the data representing
+					the XXXAgent to load.
+					(can either be compressed or uncompressed)
+		*/
+		XXXAgent(std::istream& is);
+		
+		
+		/**
+			\brief	Destructor.
+		*/
+		~XXXAgent();
+
+
+		// =================================================================
+		//	Public static methods
+		// =================================================================
+		/**
+			\brief	Return the string representation of this class name.
+			
+			\return	The string representation of this class name.
+		*/
+		static std::string toString()
+		{
+		   return "XXXAgent";
+		}
+
+
+		// =================================================================
+		//	Public methods
+		// =================================================================
+		/**
+			\brief	Return the action to perform on the currently
+					associated MDP from state 'x'.
+			
+			\param[xt	The state from which a decision has to be made.
+			
+			\return	The action to perform.
+		*/
+		int getAction(int xt) const throw (MDPException);
+		
+		
+		/**
+			\brief	Learn from <x, u, y, r> transition (online).
+			
+			\param[x	The origin state.
+			\param[u	The action performed.
+			\param[y	The state reached.
+			\param[r	The reward observed.
+		*/
+		void learnOnline(int x, int u, int y, double r) throw (MDPException);
+
+		
+		/**
+			\brief	Reset this agent.
+					(called when this Agent is associated to a new MDP).
+		*/
+		void reset() throw (MDPException);
+		
+		
+		/**
+               \brief    Free unnecessary data.
+                         (called when this Agent has finished to interact
+                         with the current MDP).
+		*/
+		void freeData();
+
+
+		/**
+			\brief	Return a clone of this Agent.
+			
+			\return	A clone of this Agent.
+		*/
+		Agent* clone() const
+		{
+		     /* TODO - call 'Serializable::checkIn<>()' in 'dds::init()' */
+			return cloneInstance<XXXAgent>(this);
+		}
+		
+		
+		/**
+			\brief	Return the name of the class of this object.
+					This method should be overloaded by any derived class.
+			
+			\return	The name of the class of this object.
+		*/
+		std::string getClassName() const { return XXXAgent::toString(); }
+
+		
+		/**
+			\brief	Serialize this Object.
+					If overloaded, the new 'serialize()' method should
+					call the 'serialize()' method of the base class
+					before doing anything else.
+		*/
+		void serialize(std::ostream& os) const;
+		
+		
+		/**
+			\brief	Deserialize this Object.
+					If overloaded, the new 'deserialize()' method should
+					call the 'deserialize()' method of the base class
+					before doing anything else.
+		*/
+		void deserialize(std::istream& is) throw (SerializableException);
+
+
+	private:
+		// =================================================================
+		//	Private attributes
+		// =================================================================
+		/* TODO - insert your attributes here */
+		
+		
+		// =================================================================
+		//	Private methods
+		// =================================================================
+		/**
+			\brief			Learn from a MDP distribution offline.
+
+							Consist to learn an appropriate initial
+							model.
+			
+			\param[mdpDistrib	A MDP distribution.
+		*/
+		void learnOffline_aux(const MDPDistribution* mdpDistrib)
+											throw (AgentException);
+
+		
+		#ifndef NDEBUG
+		/**
+			\brief	Function to call whenever the internal data could
+					be corrupted by wrong data entries
+					(calls 'assert()' for each data).
+		*/
+		void checkIntegrity() const;
+		#endif
+};
+
+#endif
